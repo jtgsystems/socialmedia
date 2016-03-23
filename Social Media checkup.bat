fd19a66b3918be2e0bbacc564ba9b368e563806c
JTG SYSTEMS <jtgsystems@gmail.com>
2016-03-23 16:40:50 -0400

    Added files via upload

---
+++ Social Media checkup.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start chrome http://facebook.com
+start chrome http://twitter.com
+start chrome http://linkedin.com/
+start chrome https://plus.google.com/
+start chrome https://www.instagram.com/jtgsystems/
