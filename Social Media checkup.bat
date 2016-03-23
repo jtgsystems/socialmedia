83a42763089ce3cc85f4a789952aad551551912f
JTG SYSTEMS <jtgsystems@gmail.com>
2016-03-23 16:41:56 -0400

    Update Social Media checkup.bat

--- Social Media checkup.bat
+++ Social Media checkup.bat
@@ -4,4 +4,4 @@ start chrome http://facebook.com
 start chrome http://twitter.com
 start chrome http://linkedin.com/
 start chrome https://plus.google.com/
-start chrome https://www.instagram.com/jtgsystems/
+start chrome https://www.instagram.com/
